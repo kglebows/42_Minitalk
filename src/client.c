37ae00480398da3b86eae4311d64e0574c53454b
Konrad Glebowski <kglebows@2-G-1.42heilbronn.de>
2023-08-25 22:04:50 +0200

    bit transfer

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: kglebows <kglebows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 13:26:44 by kglebows          #+#    #+#             */
-/*   Updated: 2023/08/24 16:28:39 by kglebows         ###   ########.fr       */
+/*   Updated: 2023/08/25 21:50:21 by kglebows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,33 @@
 
 int main(int argn, char *argc[])
 {
-	int		server_pid;
+	int			server_pid;
+	char		c;
+	int			i;
 
 	if (argn < 2)
 		return (0);
 	server_pid = ft_atoi(argc[1]);
-	ft_printf("server PID: %d", server_pid);
-	kill(server_pid, SIGUSR2);
-	usleep(100);
-	kill(server_pid, SIGUSR1);
-	// kill(server_pid, SIGUSR2);
-	// kill(server_pid, SIGUSR2);
+	// ft_printf("server PID: %d", server_pid);
+	while (*argc[2])
+	{
+		c = *argc[2]++;
+		i = 8;
+		while (i--)
+		{
+			if (c >> i & 1)
+			{
+				ft_printf("1");
+				kill(server_pid, SIGUSR2);
+			}
+			else
+			{
+				ft_printf("0");
+				kill(server_pid, SIGUSR1);
+			}
+			usleep(100);
+		}
+		ft_printf("%c", c);
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: kglebows <kglebows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 13:12:46 by kglebows          #+#    #+#             */
-/*   Updated: 2023/08/24 15:43:16 by kglebows         ###   ########.fr       */
+/*   Updated: 2023/08/25 22:03:21 by kglebows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,33 @@
 
 void act(int signo, siginfo_t *siginfo, void *context)
 {
+	int					server_pid;
+	static unsigned char			c = 0;
+	static int			i = 0;
+
 	(void)context;
+	server_pid = siginfo->si_pid;
 	if (signo == SIGUSR1)
 	{
-		ft_printf("GOT SIGUSR1!!! from %d\n", siginfo->si_pid);
+		ft_printf("0");
 	}
 	if (signo == SIGUSR2)
 	{
-		ft_printf("GOT SIGUSR2!! from %d\n", siginfo->si_pid);
+		ft_printf("1");
 	}
+	c |= (signo == SIGUSR2);
+	if (++i != 8)
+	{
+		c <<= 1;
+	}
+	else
+	{
+		ft_printf("%c", c);
+		ft_putchar_fd(c, 1);
+		i = 0;
+		c = 0;
+	}
+	
 }
 
 int main(void)
@@ -31,7 +49,7 @@ int main(void)
 	struct sigaction		action;
 
 	ft_printf("PID: %d\n", getpid());
-	action.sa_sigaction = act;
+	action.sa_sigaction = act; 
 	action.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
